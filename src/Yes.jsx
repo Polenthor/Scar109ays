882d395f27d3de728e40f9fe6edae8f1aca52a26
Aboobacker <shiduaboobacker@gmail.com>
2025-12-26 05:26:15 +0400

    Update Yes.jsx

--- src/Yes.jsx
+++ src/Yes.jsx
@@ -6,7 +6,7 @@ const Yes = () => {
     <div><h1 class="title">Great! </h1>
       <h1 class="title">Now, Are You Ready For Some questions </h1>
     
-  <Link to="/yess"><button >Yes</button></Link>  <span style={{ fontSize: "1.5rem", margin: "0 10px" }}>Or</span> <Link to="/question"><button >No</button></Link>
+  <Link to="/yess"><button >Yes</button></Link> 
 
 
     
